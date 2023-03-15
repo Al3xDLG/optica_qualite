21b524aa0a641d6141809b5f64c589a647923346
Daniel Alejandro Lopez Garcia <danyalexlg@gmail.com>
2023-03-14 20:36:02 -0600

    Modificacion CVLC

--- ProyectoOptica/optica_core/src/com/glasscode/oq/core/ControllerVentasLC.java
+++ ProyectoOptica/optica_core/src/com/glasscode/oq/core/ControllerVentasLC.java
@@ -82,62 +82,6 @@ public class ControllerVentasLC {
         }
         return r;
     }
-    
-    public boolean generarVentaLCt(DetalleVPre dvpr){
-        boolean r = false;
-        //Con este objeto nos vamos a conectar a la Base de Datos:
-        ConexionMySQL connMySQL = new ConexionMySQL();
-
-        //Abrimos la conexión con la Base de Datos:
-        Connection conn = connMySQL.open();
-        ResultSet rs = null;
-        Statement stmt = null;
-        
-        try {
-            conn.setAutoCommit(false);
-            stmt = conn.createStatement();
-
-            String query1 = "INSERT INTO venta (idEmpleado, clave) VALUES ("
-                    + dvpr.getVenta().getEmpleado().getIdEmpleado() + ",'"
-                    + dvpr.getVenta().getClave() + "')";
-            stmt.execute(query1);
-
-            String query2 = "SELECT LAST_INSERT_ID()";
-            rs = stmt.executeQuery(query2);
-
-            if (rs.next()) {
-                dvpr.getVenta().setIdVenta(rs.getInt(1));
-
-            }
-
-            for (VentaPresupuesto VentaPre : dvpr.getListaVPre()) {
-                String query3 = "INSERT INTO presupuesto (idExamenVista, clave) VALUES("
-                        + VentaPre.getVenta().get + ","
-                        + VentaPre.getPresupuesto().getIdPrespuestoLentesContacto() + ","
-                        + VentaPre.getCantidad() + ")";
-                stmt.execute(query3);
-            }
-            conn.commit();
-            conn.setAutoCommit(true);
-            rs.close();
-            stmt.close();
-            conn.close();
-            r = true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            try {
-                conn.rollback();
-
-                rs.close();
-                stmt.close();
-                conn.close();
-                connMySQL.close();
-            } catch (SQLException ex) {
-                ex.printStackTrace();
-            }
-        }
-        return r;
-    }
 
     public void generarPresupuestoLC(PresupuestoLentesdeContacto plc) {
         boolean r = false;
